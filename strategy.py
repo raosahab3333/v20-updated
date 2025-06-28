9edad81c7aaef3ee84523fe42f67eff2b6c6d302
raosahab3333 <vy807924@gmail.com>
2025-06-28 22:16:07 +0530

    Update strategy.py

--- strategy.py
+++ strategy.py
@@ -9,12 +9,37 @@ START_DATE = (datetime.today() - timedelta(days=3*365)).strftime('%Y-%m-%d')
 END_DATE = datetime.today().strftime('%Y-%m-%d')
 
 all_stocks = [
-    "RELIANCE", "INFY", "ITC"  # Trimmed for testing/demo purposes
+    "RELIANCE", "INFY", "ITC", "3MINDIA", "AHLUCONT", "AIAENG", "AJANTPHARM", "AKZOINDIA", "ALKEM", "ANANDRATHI",
+    "ANGELONE", "APARINDS", "APOLLOHOSP", "ARCHEM", "ASIANPAINT", "ASTRAL", "ASTRAZEN",
+    "AWL", "AVANTIFEED", "BAJAJ-AUTO", "BAJAJHLDNG", "BASF", "BAYERCROP", "BEL", "BERGEPAINT",
+    "BIKAJI", "BLUESTARCO", "BOSCHLTD", "BSOFT", "BSE", "CAPLIPOINT", "CARBORUNIV", "CAMS",
+    "CASTROLIND", "CELLO", "CERA", "CHAMBLFERT", "CIPLA", "CMSINFO", "COALINDIA", "COCHINSHIP",
+    "COLPAL", "CONCORDBIO", "COROMANDEL", "CROMPTON", "CRISIL", "CUMMINSIND", "DABUR", "DBCORP",
+    "DEEPAKNTR", "DHANUKA", "DIXON", "DMART", "DRREDDY", "ECLERX", "EICHERMOT", "EIDPARRY",
+    "EIHOTEL", "ELECON", "EMAMILTD", "ENGINERSIN", "ERIS", "FINEORG", "FORCEMOT", "FORTIS",
+    "GANESHHOUC", "GARFIBRES", "GHCL", "GILLETTE", "GLAXO", "GODFRYPHLP", "GODREJCP", "GODREJIND",
+    "GRINDWELL", "GRSE", "GSPL", "GUJGASLTD", "HAL", "HAPPYFORGE", "HAVELLS", "HCLTECH",
+    "HEROMOTOCO", "HINDUNILVR", "HONAUT", "ICICIGI", "IEX", "IGL", "IMFA", "INDHOTEL", "INDIAMART",
+    "INFY", "INGERRAND", "INTELLECT", "IONEXCHANG", "IRCTC", "ITC", "JBCHEPHARM", "JAIBALAJI",
+    "JIOFIN", "JWL", "JYOTHYLAB", "JYOTICNC", "KAJARIACER", "KAMS", "KFINTECH", "KEI", "KIRLOSBROS",
+    "KPIGREEN", "KPITTECH", "KPRMILL", "KSCL", "LALPATHLAB", "LICI", "LTIM", "LTTS", "MAHAPEXLTD",
+    "MAHSEAMLES", "MANKIND", "MANINFRA", "MARICO", "MARUTI", "MCX", "MCDHOLDING", "MEDANTA", "MGL",
+    "MISHTANN", "MPHASIS", "MRF", "MSUMI", "NAM-INDIA", "NATCOPHARM", "NBCC", "NEULANDLAB", "NEWGEN",
+    "NESCO", "NIITLTD", "NMDC", "OFSS", "PAGEIND", "PETRONET", "PFIZER", "PGHH", "PGHL", "PIDILITIND",
+    "PIIND", "POLYCAB", "POLYMED", "RADICO", "RAILTEL", "RATNAMANI", "RELAXO", "RITES", "ROUTE",
+    "SANOFI", "SCHAEFFLER", "SEQUENT", "SHARDAMOTR", "SHAREINDIA", "SHRIPISTON", "SIEMENS", "SKFINDIA",
+    "STYRENIX", "SUMICHEM", "SUNTV", "SUPREMEIND", "SURYAROSNI", "TANLA", "TATAELXSI", "TATAMOTORS",
+    "TATATECH", "TBOTEK", "TEAMLEASE", "TECHM", "TIINDIA", "TIMKEN", "TITAGARH", "TRITURBINE",
+    "UBL", "ULTRACEMCO", "UNITDSPR", "UPL", "URJAGLO", "USHAMART", "UTIAMC", "VBL", "VESUVIUS",
+    "VOLTAMP", "VSTIND", "WSTCSTPAPR", "ZENSARTECH", "ZFCVINDIA", "ZENTEC"
+]
+  # Trimmed for testing/demo purposes
 ]
 
 # Mock download function to simulate yfinance behavior in unsupported environments
 def download_data(symbol: str) -> pd.DataFrame | None:
     try:
+        df = yf.Ticker(symbol + ".NS").history(start=START_DATE, end=END_DATE)
         # Generate 1000 days of fake price data
         dates = pd.date_range(start=START_DATE, end=END_DATE)
         prices = pd.Series(100 + (pd.Series(range(len(dates))) % 10).cumsum(), index=dates)
